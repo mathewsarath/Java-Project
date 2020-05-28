f771e1842f7acd877f0c60507151344213ced51a
Sarath Alex <mathewsarath225@gmail.com>
2020-05-28 16:38:57 +0530

    added new package Frames and included jdbc jar file

--- Hospitalmanagement2/src/hospitalmanagement2/Hospitalmanagement2.java
+++ Hospitalmanagement2/src/hospitalmanagement2/Hospitalmanagement2.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package hospitalmanagement2;
+import Frames.*;
 
 /**
  *
@@ -16,6 +17,33 @@ public class Hospitalmanagement2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(DashBoard.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DashBoard().setVisible(true);
+            }
+        });
     }
     
 }
